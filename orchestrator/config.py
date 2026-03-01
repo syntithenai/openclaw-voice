297c6e8ed8d3e545d05f6bb6c70ce4cafbc46e2f
Steve Ryan <=>
2026-03-01 20:07:30 +1100

    Configure whisper and piper services to use 10.0.1.249 as default

--- orchestrator/config.py
+++ orchestrator/config.py
@@ -58,8 +58,8 @@ class VoiceConfig(BaseSettings):
     cut_in_pre_roll_ms: int = Field(100)
 
     # Services
-    whisper_url: str = Field("http://localhost:10000")
-    piper_url: str = Field("http://localhost:10001")
+    whisper_url: str = Field("http://10.0.1.249:10000")
+    piper_url: str = Field("http://10.0.1.249:10001")
     piper_voice_id: str = Field("en_US-amy-medium")
     piper_speed: float = Field(1.0)
     gateway_ws_url: str = Field("", validation_alias=AliasChoices("GATEWAY_WS_URL"))
